406efc60644bef069094713731bcd0d4c1029f5f
andre-ramos <33959537+andre-ramos@users.noreply.github.com>
2023-09-15 20:51:07 -0300

    Add files via upload

--- fetch_p2p_price.py
+++ fetch_p2p_price.py
@@ -1,12 +1,12 @@
 import requests
 import os
 
-headers = '{Authorization: Bearer' +  os.environ['API_KEY']}
+headers = {'Authorization: Bearer' +  os.environ['API_KEY']}
 query = {
   "filters": {
     "country": "BR"
   }
 }
-r = requests.get("https://hodlhodl.com/api/v1/offers?filters[filters.country]=BR", headers = headers, params=query)
+r = requests.get("https://hodlhodl.com/api/v1/offers", headers = headers, params=query)
 response = requests.get()
 print(response)
