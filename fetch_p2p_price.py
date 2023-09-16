fbcbaf06262716e439f64db1a3cc9f25c9556abf
Andre <33959537+andre-ramos@users.noreply.github.com>
2023-09-16 13:29:52 -0300

    Update fetch_p2p_price.py

--- fetch_p2p_price.py
+++ fetch_p2p_price.py
@@ -38,7 +38,7 @@ print(prices)
 
 res = int(cotacao_bitcoin) + 5000    
 for item in prices:
-    if item['price'] >= res:
+    if item['price'] <= res:
         rating = float(item['rating']) * 5
         msg = f""" 📣 NOVA OFERTA BARATA NA HODLHODL 📣  
             👤 USUÁRIO: {item['login']}
