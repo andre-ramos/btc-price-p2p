fa2830be714b8a24c29ca46d8bd8ec4bfb1339a9
Andre <33959537+andre-ramos@users.noreply.github.com>
2023-09-16 12:20:45 -0300

    Update fetch_p2p_price.py

--- fetch_p2p_price.py
+++ fetch_p2p_price.py
@@ -8,7 +8,6 @@ def send_to_telegram(message):
     apiToken = os.getenv('BOT_KEY')
     chatID = '47651571'
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
-    print(apiURL)
 
     try:
         response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
@@ -19,28 +18,30 @@ def send_to_telegram(message):
 cotacoes = requests.get("https://economia.awesomeapi.com.br/last/BTC-BRL")
 cotacoes = cotacoes.json()
 cotacao_bitcoin = cotacoes['BTCBRL']["bid"]
-print("Aqui"+ os.getenv('API_KEY'))
 headers = {'Authorization': 'Bearer ' +  os.getenv('API_KEY')}
 r = requests.get("https://hodlhodl.com/api/v1/offers?[side]=buy&filters[currency_code]=BRL", headers = headers)
 prices = []
 data = json.loads(r.content)
 for offer in data['offers']:
     if offer['trader']['trades_count'] > 0 and float(cotacao_bitcoin) <= float(offer['price']) :
-        prices.append(float(offer['price']))
+        prices.append({"id" :offer['id'],
+                      "price" :float(offer['price'],
+                      "login" :offer['trader']['login']
+                      "rating":offer['trader']['rating']))
 avg = sum(prices)/len(prices)
 print(f"PREÇO BTC: {cotacao_bitcoin}")
 print(prices)
 
-for price in prices:
-    res = int(cotacao_bitcoin) + 5000    
-    if price >= res:
-        rating = float(offer['trader']['rating']) * 5
+res = int(cotacao_bitcoin) + 5000    
+for item in prices:
+    if item['price'] >= res:
+        rating = float(item['rating']) * 5
         msg = f""" 📣 NOVA OFERTA BARATA NA HODLHODL 📣  
-            👤 USUÁRIO: {offer['trader']['login']}
+            👤 USUÁRIO: {item['login']}
             ⭐️ RATING: {str(rating)}
-            _PREÇO BTC: R$ {cotacao_bitcoin},
-            _PREÇO DO VENDEDOR: {price}
-            🔗 https://hodlhodl.com/offers/{offer['id']}"""
+            🔍PREÇO BTC: R$ {cotacao_bitcoin},
+            🔍PREÇO DO VENDEDOR: {item['price']}
+            🔗 https://hodlhodl.com/offers/{item['id']}"""
         send_to_telegram(msg)
     #else:
     #    send_to_telegram(f"PREÇO BITCOIN AGORA: R$ {cotacao_bitcoin}")
